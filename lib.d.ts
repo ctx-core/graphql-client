2b283c850c6c4cfdaf01ff7ef722ccf673568436
Brian Takita <brian.takita@gmail.com>
2020-05-24 13:24:37 -0700

    BREAKING CHANGE: _fetch__graphql(http_opts__1?: FetchHttpOpts): fetch__graphql(body: string, http_opts__2:FetchHttpOpts)
    feat: FetchHttpOpts
    
    feat: FetchHttpHeaders

--- lib.d.ts
+++ lib.d.ts
@@ -1,4 +1,11 @@
-export declare function _fetch__graphql(url: any, headers__1?: {}, http_opts__1?: {}): (body: any, headers__2?: {}, http_opts__2?: {}) => Promise<any>;
+export interface FetchHttpHeaders {
+    'Content-Type'?: string;
+}
+export interface FetchHttpOpts {
+    url?: string;
+    headers?: FetchHttpHeaders;
+}
+export declare function _fetch__graphql(http_opts__1?: FetchHttpOpts): (body: string, http_opts__2?: FetchHttpOpts) => Promise<any>;
 export declare function _url__graphql(host?: string): string;
 export declare function _txt__port__graphql(port?: string): string;
-export declare const fetch__graphql: (body: any, headers__2?: {}, http_opts__2?: {}) => Promise<any>;
+export declare const fetch__graphql: (body: string, http_opts__2?: FetchHttpOpts) => Promise<any>;
--- lib.ts
+++ lib.ts
@@ -1,13 +1,23 @@
 import { assign } from '@ctx-core/object'
 import { _has__dom } from '@ctx-core/dom'
 import { fetch } from '@ctx-core/fetch'
-export function _fetch__graphql(url, headers__1 = {}, http_opts__1 = {}) {
-	return async function fetch__graphql(body, headers__2 = {}, http_opts__2 = {}) {
-		const response = await fetch(url, assign({
+export interface FetchHttpHeaders {
+	'Content-Type'?:string
+}
+export interface FetchHttpOpts {
+	url?:string
+	headers?:FetchHttpHeaders,
+}
+export function _fetch__graphql(http_opts__1:FetchHttpOpts = {}) {
+	return async function fetch__graphql(
+		body:string,
+		http_opts__2:FetchHttpOpts = {},
+	) {
+		const response = await fetch(http_opts__2.url || http_opts__1.url, assign({
 			method: 'POST',
 			headers: assign({
 				'Content-Type': 'application/json',
-			}, headers__1, headers__2),
+			}, http_opts__1.headers, http_opts__2.headers),
 			body,
 		}, http_opts__1, http_opts__2))
 		if (!response.ok) {
@@ -26,6 +36,6 @@ export function _url__graphql(host = '127.0.0.1') {
 export function _txt__port__graphql(port = process.env.PORT) {
 	return (port || 80) === 80 ? '' : `:${port}`
 }
-export const fetch__graphql = _fetch__graphql(
-	_url__graphql(),
-)
+export const fetch__graphql = _fetch__graphql({
+	url: _url__graphql(),
+})
