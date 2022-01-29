23457525587ae121072a1ab229daf48cdfb41070
Brian Takita <brian.takita@gmail.com>
2022-01-29 02:25:24 -0600

    src/index: order imports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 export * from './_types'
+export * from './graphql_fetch'
 export * from './graphql_fetch_'
 export * from './graphql_port_txt_'
 export * from './graphql_url_'
-export * from './graphql_fetch'
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
+export * from './graphql_fetch.js'
 export * from './graphql_fetch_.js'
 export * from './graphql_port_txt_.js'
 export * from './graphql_url_.js'
-export * from './graphql_fetch.js'
